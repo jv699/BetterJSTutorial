13a0709187b997ff08d01bd28faad632177a1a0b
jv699 <your_email_address@example.com>
2023-05-11 19:28:02 -0400

    Better JS Tutorial

---
+++ index.js
@@ -0,0 +1,41 @@
+const BASE_SALARY = 16000;
+const SALARY_MULTIPLIER = 4;
+
+const makePerson = ({firstName, age, job, lastName}) => {
+    return {
+        name : firstName + ' ' + lastName,
+        age,
+        job
+    }
+}
+
+
+
+const tempName = {
+    firstName : 'josh',
+    lastName : 'vacarchuk' ,
+    age : 23,
+    job : 'java dev',
+    salary : BASE_SALARY * SALARY_MULTIPLIER
+}
+
+// Incorrect 
+// const hireDev = ({ devInfo }) => {
+//     const hiredDevInfo = {
+//         hired : true,
+//         ...devInfo
+//     }
+//     return hiredDevInfo;
+// }
+
+//correct
+const hireDev = ({ dev}) => {
+    const hiredDev= {
+        hired : true,
+        ...dev
+    }
+    return hiredDev;
+}
+
+const dev = makePerson(tempName);
+console.log(hireDev({ dev }));
