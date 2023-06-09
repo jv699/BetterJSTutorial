2580610759d4eda046bb5231bdd4630d286d5dc6
jv699 <jvacarch@gmail.com>
2023-06-08 21:15:06 -0400

    set timeouts and intro to promises

--- dom2.js
+++ dom2.js
@@ -1,3 +1,5 @@
+//event listeners and timeouts
+
 //generate random color
 // // RBG (0-255)
 //apply to background
@@ -16,10 +18,22 @@ const applyColorToBody = () => document.body.style.backgroundColor = createColor
 
 applyColorToBody(); // this isn't exactly how the guy did it, but I like it
 
-const newColors = document.getElementById("new-colors");
+const newColors = document.getElementById("new-colors"); //gets html element and puts it in a variable
 //on click attaches one event
 // newColors.onclick = () => applyColorToBody();
 
 //this is a preferable solution because you can add as many event listeners as you want
 newColors.addEventListener('click', applyColorToBody); //you exclude the () for some reason
-newColors.addEventListener('mouseover', applyColorToBody);
+// newColors.addEventListener('mouseover', applyColorToBody);
+
+const log = () => console.log('in timeout');
+//adding a () evaluates the function, then uses the result for set timeout
+//just using the name will evaluate it after the timer
+// setTimeout(applyColorToBody , 5000); 
+console.log('ran here'); //settimeout is not blocking, code continues to evaluates
+//setTimeout happens once
+
+//set interval happens over and over
+const interval = setInterval(applyColorToBody, 2000);
+//clearInterval allows us to stop the shifting
+newColors.addEventListener('click', () => clearInterval(interval));
---
+++ index3.js
@@ -0,0 +1,34 @@
+//Promises tutorial
+
+//made up code
+// const chargeCard = () => null;
+
+// chargeCard('123456789', () => {
+//     //this is a callback, it runs when chargeCard is done running
+//     saveToDB(result, () => {
+//         //another callback, callback hell, especially with returns
+//     })
+// });
+
+//Promises can be used in place of a callback, works like the name
+//Most APIs return promises, so good to learn
+//using the example from above
+
+//promise is given code to be run out of order, or rather independently of everything else
+const chargeCard = () => new Promise((resolve, reject) => {
+    //card is valid
+    reject('FAIL'); //this skips the 'then' state and goes right to 'reject'
+    if (true) {
+        return resolve(true);
+    }
+    return resolve(false); //no error, just a declined card
+    //rejecting is used if there is something wrong
+});
+
+chargeCard().then((val) => {
+    //then runs when the promise is resolved
+    console.log(val);
+}).catch((err) => {
+    //this is used if it returns a rejection 
+    console.log(err);
+});
